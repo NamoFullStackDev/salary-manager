e65e6ced66d54e0d4ebb237724b309dd0570bd5b
Naman <namanror0402@gmail.com>
2026-05-05 13:50:06 +0530

    feat: implement minimal employees create and index endpoints (green)

---
+++ app/controllers/api/v1/employees_controller.rb
@@ -0,0 +1,31 @@
+class Api::V1::EmployeesController < ApplicationController
+  def index
+    employees = Employee.all
+
+    render json: { data: employees }, status: :ok
+  end
+
+  def create
+    employee = Employee.new(employee_params)
+
+    if employee.save
+      render json: { data: employee }, status: :created
+    else
+      render json: { errors: employee.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def employee_params
+    params.require(:employee).permit(
+      :full_name,
+      :email,
+      :job_title,
+      :country,
+      :salary,
+      :currency,
+      :employment_type
+    )
+  end
+end
